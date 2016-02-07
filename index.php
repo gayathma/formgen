efdf01e9e78155c5b156e9b5076cb845f65ca778
gayathma <gayathma3@gmail.com>
2016-02-07 18:45:56 +0530

    init commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="../FormGen/assets/css/bootstrap.min.css" >
+        <title>FormGen</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="starter-template">
+        <h1>FormGen</h1>
+        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+      </div>
+            
+        <form class="form-control" name="gen_form" id="gen_form" method="POST" action="generator.php">
+            
+        </form>
+        </div>
+    </body>
+</html>
