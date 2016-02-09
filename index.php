6a33bbc9cb9e6f69dd81d0fd706d21cd6d7a0fa8
shamain <shamain.kolourbox@gmail.com>
2016-02-09 22:25:20 +0530

    asd
    ad

--- index.php
+++ index.php
@@ -3,23 +3,36 @@
     <?php include 'header.php';?>
     <body>
         <div class="container">
-            <div class="starter-template">
-                <h1>FormGen</h1>
-                <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-                <a class="btn btn-success" href="index.php">New Form</a>
+ <div class="row">
+<div class="col-md-12">
+ <h1>Form Generator</h1>
+                <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.<a class="btn btn-success" href="index.php" style="float:right">New Form</a></p>
+                
+</div>
+ </div>
+ 
+        <div class="row">
+        <form name="gen_form" id="gen_form" method="POST" action="generator.php" >
+            <div class="col-md-4">
+ <div class="form-group">
+         <label class="form-label">Form Title</label>
+                        <input type="text" class="form-control" id="form_name" name="form_name" placeholder="Form Title">
+                  
+                </div>
             </div>
 
-            <form class="form-horizontal" name="gen_form" id="gen_form" method="POST" action="generator.php">
-                <div class="form-group">
-                    <div class="col-md-12">
-                        <input type="text" class="form-control" id="form_name" name="form_name" placeholder="Form Title">
-                    </div>
+            <div class="col-md-4">
+ <div class="form-group">
+  <label class="form-label">Element Name</label>
+ <input type="text" class="form-control" name="element_name" id="element_name" placeholder="Label">
+                 
+                  
                 </div>
-                <div class="form-group">
-                    <div class="col-md-6">
-                        <input type="text" class="form-control" name="element_name" id="element_name" placeholder="Label">
-                    </div>
-                    <div class="col-md-6">
+            </div>
+
+              <div class="col-md-4">
+               <div class="form-group">
+                 <label class="form-label">Element Type</label>
                         <select class="form-control" id="elemet_type" name="elemet_type">
                             <option value="txtf">Text Field</option>
                             <option value="txtarea">Text Area</option>
@@ -29,20 +42,35 @@
                             <option value="sub_btn">Submit Button</option>
                             <option value="canc_btn">Cancel Button</option>
                         </select>
+                        </div>
                     </div>
-                </div>
-                <div class="form-group">
-                    <div class="col-md-6">
+
+
+               <div class="col-md-4">
+  <div class="form-group">
+                           <label class="form-label">Select Options</label>
                         <input type="text" class="form-control"  name="options" id="options" placeholder="Select Options with comma (,) seperated">
                     </div>
+              
+            </div>
+<div class="col-md-4">
+             <div class="form-group">
+                      <label class="form-label" style="display: block;height: 20px;"></label>
+                        <button type="submit" class="btn btn-info">Ok</button>
+                   
                 </div>
-                <div class="form-group">
-                    <div  style="float: right">
-                        <button type="submit" class="btn btn-default">Ok</button>
-                    </div>
                 </div>
                 <input name="function" value="generate" type="hidden"/>
-            </form>                       
+                   </form>
+        </div>
+           
+     
+         
+            
+               
+               
+               
+                                 
         </div>
     </body>
 </html>
