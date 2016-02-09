8a43b7f1f12e22250e9bcae34abd198a3feed694
gayathma <gayathma3@gmail.com>
2016-02-09 23:04:56 +0530

    db

--- formgen.sql
+++ formgen.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 09, 2016 at 05:37 PM
+-- Generation Time: Feb 09, 2016 at 06:34 PM
 -- Server version: 5.5.27
 -- PHP Version: 5.4.7
 
@@ -30,27 +30,7 @@ CREATE TABLE IF NOT EXISTS `form` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `Title` varchar(500) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
-
---
--- Dumping data for table `form`
---
-
-INSERT INTO `form` (`id`, `Title`) VALUES
-(1, 'fdsfsd'),
-(2, 'fsdf'),
-(3, 'fsdf'),
-(4, 'czxcx'),
-(5, 'czxcx'),
-(6, 'fdsfd'),
-(7, 'dfsd'),
-(8, 'dfsd'),
-(9, 'cxvc'),
-(10, 'vvx'),
-(11, 'cxzc'),
-(12, 'fdsfsdfsdf'),
-(13, 'dfds'),
-(14, 'cxzcxc');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -65,32 +45,7 @@ CREATE TABLE IF NOT EXISTS `form_element` (
   `element_label` varchar(500) DEFAULT NULL,
   `options` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=20 ;
-
---
--- Dumping data for table `form_element`
---
-
-INSERT INTO `form_element` (`id`, `form_id`, `element`, `element_label`, `options`) VALUES
-(1, 1, 'txtf', 'dsfdf', ''),
-(2, 2, 'txtarea', 'fsdf', ''),
-(3, 3, 'txtarea', 'fsdf', ''),
-(4, 4, 'txtf', 'czxcx', ''),
-(5, 5, 'txtf', 'czxcx', ''),
-(6, 6, 'txtf', 'fsdfd', ''),
-(7, 7, 'txtarea', 'sdfsd', ''),
-(8, 8, 'txtarea', 'sdfsd', ''),
-(9, 9, 'txtarea', 'vxcv', ''),
-(10, 10, 'txtf', 'vxcv', ''),
-(11, 11, 'txtarea', 'czxcx', ''),
-(12, 12, 'txtf', 'fsdf', ''),
-(13, 13, 'txtarea', 'fsdf', ''),
-(14, 14, 'drp_dwn', 'czxcx', 'czxczxcx'),
-(15, 9, 'drp_dwn', 'gdfgf', 'xczxc,fdhhdfh'),
-(16, 9, 'drp_dwn', 'gdfgf', 'xczxc,fdhhdfh'),
-(17, 9, 'drp_dwn', 'cxzc', 'cxzcx,dgdgsd,gdgdfgf'),
-(18, 9, 'drp_dwn', 'cxzc', 'cxzcx,dgdgsd,gdgdfgf'),
-(19, 9, 'drp_dwn', 'cxzc', 'cxzcx,dgdgsd,gdgdfgf');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
