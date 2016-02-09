772f386d11f74ed3e2de7b8d7fc1e201d2aeb642
gayathma <gayathma3@gmail.com>
2016-02-09 23:12:59 +0530

    index

--- index.php
+++ index.php
@@ -1,69 +1,71 @@
-<!DOCTYPE html>
-<html>
-    <?php include 'header.php'; ?>
-    <?php  session_start();?>
-    <body>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <h1>Form Generator</h1>
-                    <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.<a class="btn btn-success" href="index.php" style="float:right">New Form</a></p>
+<?php
 
-                </div>
-            </div>
+echo '<!DOCTYPE html>';
+echo '<html>';
+include 'header.php';
+session_start();
+echo '<body>';
+echo '<div class="container">';
+echo ' <div class="row">';
+echo '    <div class="col-md-12">';
+echo '      <h1>Form Generator</h1>';
+echo '      <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.<a class="btn btn-success" href="index.php" style="float:right">New Form</a></p>';
 
-            <div class="row">
-                <form name="gen_form" id="gen_form" method="POST" action="generator.php" >
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            <label class="form-label">Form Title</label>
-                            <input type="text" class="form-control" id="form_name" name="form_name" placeholder="Form Title">
+echo ' </div>';
+echo '</div>';
 
-                        </div>
-                    </div>
+echo '<div class="row">';
+echo '  <form name="gen_form" id="gen_form" method="POST" action="generator.php" >';
+echo ' <div class="col-md-4">';
+echo '<div class="form-group">';
+echo '  <label class="form-label">Form Title</label>';
+echo ' <input type="text" class="form-control" id="form_name" name="form_name" placeholder="Form Title">';
 
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            <label class="form-label">Element Name</label>
-                            <input type="text" class="form-control" name="element_name" id="element_name" placeholder="Label">
+echo '</div>';
+echo '</div>';
 
-
-                        </div>
-                    </div>
-
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            <label class="form-label">Element Type</label>
-                            <select class="form-control" id="elemet_type" name="elemet_type">
-                                <option value="txtf">Text Field</option>
-                                <option value="txtarea">Text Area</option>
-                                <option value="chk_box">Checkboxes</option>
-                                <option value="drp_dwn">Dropdown</option>
-                                <option value="radio">Radio Button</option>
-                                <option value="sub_btn">Submit Button</option>
-                                <option value="canc_btn">Cancel Button</option>
-                            </select>
-                        </div>
-                    </div>
+echo '<div class="col-md-4">';
+echo '    <div class="form-group">';
+echo '     <label class="form-label">Element Name</label>';
+echo '     <input type="text" class="form-control" name="element_name" id="element_name" placeholder="Label">';
 
 
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            <label class="form-label">Select Options</label>
-                            <input type="text" class="form-control"  name="options" id="options" placeholder="Select Options with comma (,) seperated">
-                        </div>
+echo ' </div>';
+echo ' </div>';
 
-                    </div>
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            <label class="form-label" style="display: block;height: 20px;"></label>
-                            <button type="submit" class="btn btn-info">Ok</button>
+echo '  <div class="col-md-4">';
+echo '   <div class="form-group">';
+echo '  <label class="form-label">Element Type</label>';
+echo '  <select class="form-control" id="elemet_type" name="elemet_type">';
+echo '      <option value="txtf">Text Field</option>';
+echo '    <option value="txtarea">Text Area</option>';
+echo '      <option value="chk_box">Checkboxes</option>';
+echo '    <option value="drp_dwn">Dropdown</option>';
+echo '     <option value="radio">Radio Button</option>';
+echo '      <option value="sub_btn">Submit Button</option>';
+echo '       <option value="canc_btn">Cancel Button</option>';
+echo '    </select>';
+echo '  </div>';
+echo ' </div>';
 
-                        </div>
-                    </div>
-                    <input name="function" value="generate" type="hidden"/>
-                </form>
-            </div>
-        </div>
-    </body>
-</html>
+
+echo '   <div class="col-md-4">';
+echo '     <div class="form-group">';
+echo '        <label class="form-label">Select Options</label>';
+echo '         <input type="text" class="form-control"  name="options" id="options" placeholder="Select Options with comma (,) seperated">';
+echo '     </div>';
+
+echo '  </div>';
+echo '  <div class="col-md-4">';
+echo '       <div class="form-group">';
+echo '        <label class="form-label" style="display: block;height: 20px;"></label>';
+echo '        <button type="submit" class="btn btn-info">Ok</button>';
+
+echo '     </div>';
+echo '   </div>';
+echo '   <input name="function" value="generate" type="hidden"/>';
+echo '  </form>';
+echo '  </div>';
+echo '</div>';
+echo '</body>';
+echo '</html>';
